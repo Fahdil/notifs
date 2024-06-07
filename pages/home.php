f814199a145f39b8b474184ecb018bcde5fbd93b
Fahdil <fahdilbello390@gmail.com>
2024-06-07 03:31:54 +0100

    cration de la page home et sons css dans les dossier corespondants

---
+++ pages/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Notifs</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/css/home.css">
+    </head>
+    <body>
+        
+    </body>
+    <footer>
+
+    </footer>
+</html>
